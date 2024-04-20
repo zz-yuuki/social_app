993a7ed076466f2245e6f715065206f798a00fa6
yuuki <kakashi16023@gmail.com>
2024-04-21 00:39:20 +0700

    add:feature createpost + bug

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,7 @@
 import { useQuery,useMutation, useQueryClient,useInfiniteQuery } from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import { createPost, createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
+import { INewPost, INewUser } from "@/types";
+import { QUERY_KEYS } from "./queryKeys";
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -12,5 +13,25 @@ export const useSignInAccount = () => {
     return useMutation({
       mutationFn: (user: { email: string; password: string; }) =>
         signInAccount(user),
-    });
-  };
+    }
+  );
+};
+
+
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount
+    }
+  );
+};
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
+  });
+};
